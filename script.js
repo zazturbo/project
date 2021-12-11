414f3379e0c20ebf490426ffb44bcfa738bde69e
Jugyn <example@gmail.com>
2021-12-12 01:01:04 +0200

    Cikl s voprosami

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const numberOfFilms = +prompt("skoljko filjmov vi uzhe posmotreli?", "");
+const numberOfFilms = +prompt("skoljko filjmov vi uzhe posmotreli?", ""); //+ zīme pirms prompt pārvērš saņemto par skaitli
 
 const personalMovieDB = {
     count: numberOfFilms,
@@ -10,12 +10,39 @@ const personalMovieDB = {
     privat: false
 };
 
-const   a = prompt("Odin iz poslednih prosmotrennih filjmov?", ""),
-        b = prompt("Na skoljko ocenite ego?", ""),
-        c = prompt("Odin iz poslednih prosmotrennih filjmov?", ""),
-        d = prompt("Na skoljko ocenite ego?", "");
+// const   a = prompt("Odin iz poslednih prosmotrennih filjmov?", ""),
+//         b = prompt("Na skoljko ocenite ego?", ""),
+//         c = prompt("Odin iz poslednih prosmotrennih filjmov?", ""),
+//         d = prompt("Na skoljko ocenite ego?", "");
 
-personalMovieDB[a] = b;
-personalMovieDB[c] = d;
+// personalMovieDB[a] = b;
+// personalMovieDB[c] = d;
+
+for (let i = 0; i <2; i++){
+    const   a = prompt("Odin iz poslednih prosmotrennih filjmov?", ""),
+            b = prompt("Na skoljko ocenite ego?", "");
+
+            if (a != null && b != null && a !="" && b!="" && a.length<50) {   //ja a nav vienāds ar null (atcēla ievadi) un a nav vienāds ar tukšumu (tukšas "" pēdiņas) un a garums nav garāks par 50
+            personalMovieDB[a] = b;
+            console.log("Done");
+
+            } else {
+                console.log("error");
+                i--;    //ja noteikumi nav izpildīti, šis ekrements atgriež atpakaļ ciklu
+            }
+
+
+}
+if  (personalMovieDB<10) {
+    console.log("Prosmotreno nalo filjmov");
+    } else if (personalMovieDB.count>=10 &&personalMovieDB.count<30) {
+    console.log("Vi klassnij zritelj");
+    } else if (personalMovieDB.count>=30) {
+    console.log("Vi kinoman!");
+    } else {
+        console.log("Proizoshla oshibka!!!");
+    }
+
+
 
 console.log(personalMovieDB);
--- script2.js
+++ script2.js
@@ -1,27 +1,2 @@
 "use strict";
 
-let num = 50;
-
-// while(num<=55){
-//     console.log(num);
-//     num++;
-// }
-
-// do {
-//     console.log(num);
-    
-// }
-// while (num<55);
-
-// for (let i = 1; i < 8; i++) {
-//     console.log(num);
-//     num++;
-// }
-
-for (let i = 1; i  <10; i++) {
-    if(i===6) {
-        // break;
-        continue;
-    }
-    console.log(i);
-}
