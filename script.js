97bbf5b6687a182ab343c3020f4f0cc8cc0bfb01
Jugyn <example@gmail.com>
2021-12-12 19:30:21 +0200

    Funkcijas

--- script.js
+++ script.js
@@ -1,36 +1,28 @@
 "use strict";
 
-let num = 20;
+const str = "test";
 
-function showFirstMessage(text) {
-    console.log(text);
-    num = 10;
-    console.log(num);
-}
+console.log(str.length);
 
-showFirstMessage("Hello world!");
-console.log(num);
+const arr = [1, 2, 4];
 
-function calc(a, b) {
-        return (a+b);
-}
+console.log(str.toUpperCase());
+console.log(str.toLowerCase());
+console.log(str);
 
-console.log(calc(4, 3));
-console.log(calc(5, 6));
-console.log(calc(10, 6));
 
-function ret() {
-    let num = 50;
-    return num;
-}
+const fruit = "Some fruit";
+console.log(fruit.indexOf("q"));
 
-const anotherNum = ret();
-console.log (anotherNum);
+const logg = "Hello world mir";
 
-const logger = function(){
-console.log("Hello");
-};
+// console.log(logg.substring(0, 5,));
+// console.log(logg.substring(0, 5,));
+console.log(logg.substr(0, 5,));
 
-logger();
+const num = 12.2;
+console.log(Math.round(num));
 
-const calc = (a, b) => {return a+b};
+const test = "12.2px";
+console.log(parseInt(test));
+console.log(parseFloat(test));
