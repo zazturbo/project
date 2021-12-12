f4cee9402c7c55e72c8fd407cd9a6494ee374820
Jugyn <example@gmail.com>
2021-12-12 19:53:06 +0200

    fail s filjmami

--- script.js
+++ script.js
@@ -1,28 +1,57 @@
-"use strict";
+/* Задание на урок:
 
-const str = "test";
+1) Первую часть задания повторить по уроку
 
-console.log(str.length);
+2) Создать функцию showMyDB, которая будет проверять свойство privat. Если стоит в позиции
+false - выводит в консоль главный объект программы
 
-const arr = [1, 2, 4];
+3) Создать функцию writeYourGenres в которой пользователь будет 3 раза отвечать на вопрос 
+"Ваш любимый жанр под номером ${номер по порядку}". Каждый ответ записывается в массив данных
+genres
 
-console.log(str.toUpperCase());
-console.log(str.toLowerCase());
-console.log(str);
+P.S. Функции вызывать не обязательно*/
 
+'use strict';
 
-const fruit = "Some fruit";
-console.log(fruit.indexOf("q"));
+let numberOfFilms;
+function start(){
+    numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+    while(numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
 
-const logg = "Hello world mir";
+    }
+}
 
-// console.log(logg.substring(0, 5,));
-// console.log(logg.substring(0, 5,));
-console.log(logg.substr(0, 5,));
+start();
 
-const num = 12.2;
-console.log(Math.round(num));
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
 
-const test = "12.2px";
-console.log(parseInt(test));
-console.log(parseFloat(test));
+for (let i = 0; i < 2; i++) {
+    const a = prompt('Один из последних просмотренных фильмов?', ''),
+          b = prompt('На сколько оцените его?', '');
+
+    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+        personalMovieDB.movies[a] = b;
+        console.log('done');
+    } else {
+        console.log('error');
+        i--;
+    }
+}
+
+if (personalMovieDB.count < 10) {
+    console.log("Просмотрено довольно мало фильмов");
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    console.log("Вы классический зритель");
+} else if (personalMovieDB.count >= 30) {
+    console.log("Вы киноман");
+} else {
+    console.log("Произошла ошибка");
+}
+
+console.log(personalMovieDB);
