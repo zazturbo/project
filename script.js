15d0d38968437b00ee0d0f43ea3585ed17ffb2fa
Jugyn <example@gmail.com>
2021-12-15 22:25:14 +0200

    darbs ar objektiem

--- script.js
+++ script.js
@@ -1,14 +1,35 @@
 'use strict';
 
-function first(){
-    //do something
-    setTimeout(function() {
-        console.log(1);
-            }, 500);
-}
+const options = {
+    name: 'test',
+    width: 1024,
+    height: 1024,
+    colors: {
+        border: 'black',
+        bg: 'red'
+    },
+    makeTest: function() {
+        console.log("Test");
+    }
+};
+options.makeTest();
 
-function second() {
-    console.log(2);
-}
-first();
-second();
+console.log(options.name);
+delete options.name;
+console.log(options);
+let counter = 0;
+for (let key in options) {
+    if (typeof(options[key]) === 'object') {
+        for (let i in options[key]) {
+            console.log(`svoistva ${i} imeet znachenie ${options[key][i]}`);
+            counter++;
+        } 
+     } else {
+    console.log(`svoistva ${key} imeet znachenie ${options[key]}`);
+    counter++;
+     }
+    }
+
+console.log(counter);
+
+console.log(Object.keys(options).length);
