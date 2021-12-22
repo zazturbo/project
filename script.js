3f4a819f02b1bd9a28bb7d18be68d8ee5578d5a1
Jugyn <example@gmail.com>
2021-12-22 22:15:11 +0200

    mans tests ar produktiem

--- script.js
+++ script.js
@@ -1,38 +1,17 @@
 'use strict';
 
-const options = {
-    name: 'test',
-    width: 1024,
-    height: 1024,
-    colors: {
-        border: 'black',
-        bg: 'red'
-    },
-    makeTest: function() {
-        console.log("Test");
+const produkti = {
+    āboli: "auglis",
+    burkāni: "dārzenis",
+    biezpiens: "piena produkts",
+    kartupeļi: "dārzenis",
+    dzērieni: {
+        fanta: "gāzēts dzēriens",
+        sula: "negāzēts dzēriens",
+        limonāde: "gāzēts dzēriens"
     }
 };
-options.makeTest();
+// console.log(produkti);
 
-const {border, bg} = options.colors;
-console.log(border);
-
-// console.log(options.name);
-// delete options.name;
-// console.log(options);
-// let counter = 0;
-// for (let key in options) {
-//     if (typeof(options[key]) === 'object') {
-//         for (let i in options[key]) {
-//             console.log(`svoistva ${i} imeet znachenie ${options[key][i]}`);
-//             counter++;
-//         } 
-//      } else {
-//     console.log(`svoistva ${key} imeet znachenie ${options[key]}`);
-//     counter++;
-//      }
-//     }
-
-// console.log(counter);
-
-// console.log(Object.keys(options).length);
+const{fanta, sula, limonāde} = produkti.dzērieni;
+console.log(limonāde);
