5c8fe5f377685f1285055c9219d4e3b377db8003
Jugyn <example@gmail.com>
2022-01-04 22:30:01 +0200

    atkartoju par objektiem

--- script2.js
+++ script2.js
@@ -1,2 +1,24 @@
 "use strict";
 
+const auto = {
+    bmw: 'luxus',
+    audi: 'luxus',
+    mb: 'luxus',
+    mazda: 'medium'
+};
+ console.log(auto);
+
+ auto.renault = 'economy';
+ console.log(auto);
+
+ const tachjka = 'ford';
+
+ auto[tachjka] = 'economy';
+
+ console.log(auto);
+
+ delete auto.mb;
+
+console.log(auto);
+
+
