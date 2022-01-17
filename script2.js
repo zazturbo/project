707ae65610ab82a7420b5cbdc00cbccc25baa6ec
Jugyn <example@gmail.com>
2022-01-17 21:38:33 +0200

    destrukturizācijas piemērs

--- script2.js
+++ script2.js
@@ -1,7 +1,22 @@
 "use strict";
 
-const name = 'Jugyn';
-const city = 'Riga';
-const greeting = `My name is ${name}, i live in ${city}!`;
+// const name = 'Jugyn';
+// const city = 'Riga';
+// const greeting = `My name is ${name}, i live in ${city}!`;
 
-console.log(greeting);
+// console.log(greeting);
+
+
+    const user = {
+        height: 180,
+        age: 35,
+        name: 'vasja',
+    };
+
+    const{height, age} = user;
+    const{name} = user;
+
+    console.log(height);
+    console.log(age);
+    console.log(name);
+    
